fcd5b33caba77855c2878192c54875b7889b3488
Adam Kruppa <adam_kruppa@epam.com>
2025-01-28 17:06:10 +0100

    change inndex

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,15 +1,18 @@
 exports.handler = async (event) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
 
+    const path = event.rawPath; // Adjust according to your event object structure
+    const httpMethod = event.requestContext.http.method; // Adjust according to your event object structure
+
     const validPath = '/hello';
     const validMethod = 'GET';
 
-    if (event.path !== validPath || event.httpMethod !== validMethod) {
+    if (path !== validPath || httpMethod !== validMethod) {
         return {
             statusCode: 400,
             body: JSON.stringify({
                 statusCode: 400,
-                message: `Bad request syntax or unsupported method. Request path: ${event.path}. HTTP method: ${event.httpMethod}.`
+                message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${httpMethod}.`
             })
         };
     }
