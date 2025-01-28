86c73940c51c8179dacbb7d249046acc00c77dab
Adam Kruppa <adam_kruppa@epam.com>
2025-01-28 16:32:48 +0100

    Init commit change

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,8 +1,25 @@
 exports.handler = async (event) => {
-    // TODO implement
+    console.log('Received event:', JSON.stringify(event, null, 2));
+
+    const validPath = '/hello';
+
+    if (event.path !== validPath) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                statusCode: 400,
+                message: 'Bad request syntax or unsupported method. Request path: ${event.path}. HTTP method: ${event.httpMethod}'
+            })
+        };
+    }
+
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify({
+            statusCode: 200,
+            message: 'Hello from Lambda'
+        })
     };
+
     return response;
 };
